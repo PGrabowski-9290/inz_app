3e8f8c9f3f3650b91e4cacd0c9245ece01f8cb54
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2022-09-10 18:59:57 +0200

    NavBar & NavBarMenu components.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from "../assets/logo_no_background.svg"
+import NavBarMenu from './NavBarMenu'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className='flex items-center justify-between text-center font-bold bg-gray-900 text-white px-2 mb-1 h-12'>
+      <div className='h-12 w-12'>
+        <Link to='/'>
+          <Logo className='max-h-full max-w-full text-white'/>
+        </Link>
+      </div>
+      <NavBarMenu />
+    </header>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { VechaiProvider } from "@vechaiui/react";
 
-import "./style.css"
 import App from "./components/App"
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
