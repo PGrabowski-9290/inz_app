d49e001d29bff5237f695b50f86a4dca1c2a5d37
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2022-10-15 21:48:56 +0200

    Update npm scripts and packages

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "./uploads")));
