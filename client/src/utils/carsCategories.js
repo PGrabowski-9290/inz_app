bfdc97c35aa328e3a6701d7a0875359a62256fa1
PGrabowski-9290 <92215276+PGrabowski-9290@users.noreply.github.com>
2022-12-25 21:33:59 +0100

    dodanie enum listy kategorii

---
+++ client/src/utils/carsCategories.js
@@ -0,0 +1,13 @@
+const carCategories = {
+  converitble: 'Convertible',
+  coupe: 'Coupe',
+  hatchback: 'Hatchback',
+  pickup: 'Pickup',
+  suv: 'SUV',
+  sedan: 'Sedan',
+  van: 'Van/Minivan',
+  wagon: 'Wagon'
+}
+
+//listing
+//Object.keys(carCategories).forEach( x => console.log(x))
