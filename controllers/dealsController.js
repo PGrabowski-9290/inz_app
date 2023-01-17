f5ba6bfae3884044a435dec73055c97e9e1d0a2f
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-01-18 00:04:22 +0100

    uzupełnianie formularzu pdf i zapisanie na dysku

--- controllers/dealsController.js
+++ controllers/dealsController.js
@@ -1,6 +1,9 @@
 const Deals = require('../models/deals');
 const Users = require('../models/user');
 const Offerts = require('../models/offerts');
+const fs = require('fs');
+const { PDFDocument, StandardFonts } = require('@visaright/pdf-lib');
+const util = require('util');
 
 const createDeal = async (req,res,next) => {
   try {
@@ -45,7 +48,52 @@ const createDeal = async (req,res,next) => {
 }
 
 const getPdf = async (req,res,next) => {
+  try {
+    
+    const data = {
+      "date": "a",
+      "city": "a",
+      "sellerName": "a",
+      "sellerPesel": "a",
+      "sellerNIP": "a",
+      "address": "a",
+      "personalId": "a",
+      "idVerBy": "a",
+      "buyerName": "a",
+      "buyerPesel": "a",
+      "buyerNIP": "a",
+      "BuyerAddress": "a",
+      "buyerId": "a",
+      "buyerIdVerBy": "a",
+      "car": "a",
+      "carYear": "a",
+      "vin": "a",
+      "numberPlate": "a",
+      "odometer": "a",
+      "price": "a"
+    }
 
+    const readFile = util.promisify(fs.readFile)
+    const file = await readFile('./config/umowa-form.pdf');
+    const pdfFile = await PDFDocument.load(file, {updateMetadata: false});
+    const font = await pdfFile.embedFont(StandardFonts.Courier);
+    const pdfForm = pdfFile.getForm();
+    Object.keys(data).forEach(el => {
+      const field = pdfForm.getTextField(el);
+      field.setText(data[el]);
+      field.updateAppearances(font);
+    })
+    pdfForm.flatten();
+    
+    const pdfBytes = await pdfFile.save()
+    fs.writeFile("filled.pdf", pdfBytes, () => {
+      console.log("PDF DONE")
+    })
+
+    res.status(200).json({message: "tak"});
+  } catch (err) {
+    next(err)
+  }
 }
 
-module.exports = {createDeal}
+module.exports = {createDeal, getPdf}
--- routes/dealsRoute.js
+++ routes/dealsRoute.js
@@ -3,6 +3,7 @@ const dealsController = require('../controllers/dealsController');
 const { verifyJWT, verifyRole } = require('../middleware/auth');
 const roles = require('../config/config').roles;
 
-router.post('/new', verifyJWT, verifyRole(roles.Admin), dealsController.createDeal);
+router.post('/new', verifyJWT, verifyRole(roles.Admin, roles.User), dealsController.createDeal);
+router.get('/generate', verifyJWT, verifyRole(roles.Admin, roles.User), dealsController.getPdf);
 
 module.exports = router;
