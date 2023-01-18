2284277918b6face50ca91c5ebdddff1c7578449
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-01-18 21:59:57 +0100

    generowanie poprawnego pdf,a z danymi z bazy

--- controllers/dealsController.js
+++ controllers/dealsController.js
@@ -1,14 +1,20 @@
 const Deals = require('../models/deals');
 const Users = require('../models/user');
 const Offerts = require('../models/offerts');
+const Settings = require('../models/settings');
 const fs = require('fs');
-const { PDFDocument, StandardFonts } = require('@visaright/pdf-lib');
+const { PDFDocument} = require('@visaright/pdf-lib');
+const fontkit = require('@pdf-lib/fontkit')
 const util = require('util');
 
 const createDeal = async (req,res,next) => {
   try {
     const data = req?.body?.data;
     if(!data) return res.status(400).json({message: "Błędne zapytanie"});
+
+    const resOffert = await Offerts.findOne({_id: data.offert});
+    if(!resOffert) return res.status(400).json({message: "Nie odnaleziono oferty"});
+    if(resOffert.isSold === true) return res.status(400).json({message: "Wybrana ofert została już sprzedana"})
 
     const number = await Deals.estimatedDocumentCount() + 1;
     const userId = await Users.findOne({email: req.email}).select('_id');
@@ -30,18 +36,10 @@ const createDeal = async (req,res,next) => {
     });
 
     const result = await newDeal.save();
-    const resOffert = await Offerts.findOneAndUpdate(
-      {_id: data.offert},
-      {
-        isSold: true
-      }
-    );
+    resOffert.isSold = true;
+    await resOffert.save();    
 
-      
-    console.log("RESULT:  ",result)
-    console.log("resOFFERT:  ",resOffert)
-
-    res.status(200).json({message: "Utworzno"})
+    res.status(200).json({id: result._id ,message: "Utworzno"})
   } catch (err) {
     next(err)
   }
@@ -49,39 +47,58 @@ const createDeal = async (req,res,next) => {
 
 const getPdf = async (req,res,next) => {
   try {
+    const dealId = req.query?.id;
+    if (!dealId) return res.status(400).json({message: "Bad Request"});
+
+    const deal = await Deals.findOne({_id: dealId}).populate({path: 'offert', populate: {path: "salons"}}).exec();
+
+    if (!deal) return res.status(400).json({message: "Błąd zapytania"});
     
+    const defValues = await Settings.findOne({id: 0});
+
+    console.log(deal);
+
     const data = {
-      "date": "a",
-      "city": "a",
-      "sellerName": "a",
-      "sellerPesel": "a",
-      "sellerNIP": "a",
-      "address": "a",
-      "personalId": "a",
-      "idVerBy": "a",
-      "buyerName": "a",
-      "buyerPesel": "a",
-      "buyerNIP": "a",
-      "BuyerAddress": "a",
-      "buyerId": "a",
-      "buyerIdVerBy": "a",
-      "car": "a",
-      "carYear": "a",
-      "vin": "a",
-      "numberPlate": "a",
-      "odometer": "a",
-      "price": "a"
+      "date": deal.dateAdd.toString(),
+      "city": deal.offert?.salons?.location?.city.toString(),
+      "sellerName": defValues.companyDetails.name.toString(),
+      "sellerPesel": "",
+      "sellerNIP": defValues.companyDetails.nip.toString(),
+      "address": defValues.companyDetails.street.toString()+" "+defValues.companyDetails.zipCode.toString()+" "+defValues.companyDetails.city.toString(),
+      "personalId": "-",
+      "idVerBy": "-",
+      "buyerName": deal.buyerDetails.name.toString(),
+      "buyerPesel": deal.buyerDetails?.pesel?.toString(),
+      "buyerNIP": deal.buyerDetails?.nip?.toString(),
+      "BuyerAddress": deal.buyerDetails.address.toString(),
+      "buyerId": deal.buyerDetails.personalIdNumber.toString(),
+      "buyerIdVerBy": deal.buyerDetails.personalIdVerifiedBy.toString(),
+      "car": deal.offert?.car?.make.toString()+" "+deal.offert?.car?.model.toString(),
+      "carYear": deal.offert?.car?.year.toString(),
+      "vin": deal.offert?.car?.vin.toString(),
+      "numberPlate": deal.offert?.car?.numberPlate?.toString(),
+      "odometer": deal.offert?.car?.odometer.toString(),
+      "price": deal.offert?.price.toString()
     }
 
     const readFile = util.promisify(fs.readFile)
     const file = await readFile('./config/umowa-form.pdf');
     const pdfFile = await PDFDocument.load(file, {updateMetadata: false});
-    const font = await pdfFile.embedFont(StandardFonts.Courier);
+    
+    pdfFile.registerFontkit(fontkit);
+    
+    const font = fs.readFileSync("./config/font.ttf");
+    const customFont = await pdfFile.embedFont(font)
     const pdfForm = pdfFile.getForm();
+
+    const rawUpdateFieldAppearances = pdfForm.updateFieldAppearances.bind(pdfForm);
+    pdfForm.updateFieldAppearances = function () {
+      return rawUpdateFieldAppearances(customFont);
+    };
+
     Object.keys(data).forEach(el => {
       const field = pdfForm.getTextField(el);
       field.setText(data[el]);
-      field.updateAppearances(font);
     })
     pdfForm.flatten();
     
