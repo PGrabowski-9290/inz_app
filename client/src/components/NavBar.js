a77f78ebf844ebded99b39d0a602806a7218c019
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-02-02 00:32:44 +0100

    fix zindex

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   const location = useLocation();
 
   return (
-    <nav className='bg-gray-800 fixed top-0 w-full z-10'>
+    <nav className='bg-gray-800 fixed top-0 w-full z-20'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='relative flex h-16 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center sm:items-center sm:justify-between'>
