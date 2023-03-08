d401655930636465eb12cc11aeba5843dfdb3eb7
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-03-08 18:39:38 +0100

    aktualizacja skryptu konfiguracji bazy danych

--- install.js
+++ install.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const register = require("./controllers/authController").register;
 const config = require("./config/config");
-const Settings = require('/models/settings');
-
+const Settings = require('./models/settings');
+const ModelsList = require('./models/model_list')
 const createUser = async () => {
   const req = {
     body: {
@@ -32,31 +32,61 @@ const createUser = async () => {
   console.log(req)
   return await register(req,res)
 }
+const restoreDB = async () => {
+  try {
+    const modelsData = require('./install/models.js')
+    const settings = require('./install/settings.js')
 
+    await Settings.createCollection()
+    await Settings.insertMany(settings,{limit: null, lean: true}, () => {
+      console.log("*** settings imported ***")
+    })
+    await ModelsList.createCollection()
+
+    for (const obj of modelsData ) {
+      const newModel = new ModelsList(obj)
+      await newModel.save()
+    }
+
+    console.log("   * Koniec importu")
+    return true
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
 const install = async () => {
   try {
-    //create user
-    const a = await createUser();
-    if (a) { 
-      console.log(a)
-      process.exit()
+    //restore DB
+    const db = await restoreDB()
+    if (db) {
+      console.log("* DB DONE *")
     }
-    console.log(a)
+    //create user
+    const user = await createUser();
+    if (user) {
+      console.log("* USER DONE *")
+    }
 
-    
+    return true
   } catch(err) {
-    throw err
+    console.log(err)
+    return false
   }
 }
 
-
+console.log("# Konfiguracja bazy danych")
 mongoose.connect(config.mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(async () => {
-    console.log('Połączono z bazą danych')
-    
+    console.log(' - Połączono z bazą danych')
+    const fin = await install()
+    if (fin) {
+      console.log(" - Zakończono")
+    }
+    process.exit()
   })
-  .catch(err => console.log(`Błąd połączenia z bazą danych:\n ${err}`));
+  .catch(err => console.log(` !!! Błąd połączenia z bazą danych:\n ${err}`));
 
---
+++ install/settings.js
@@ -0,0 +1,24 @@
+const settings = [{
+  "_id": {
+    "$oid": "63c46e69944ea83acd1316cf"
+  },
+  "id": 0,
+  "__v": 0,
+  "companyDetails": {
+    "name": "Firma",
+    "nip": "1233455612",
+    "city": "Miasto",
+    "zipCode": "01-233",
+    "street": "Ulica 2"
+  },
+  "contact": {
+    "phoneNumber": "123123123",
+    "email": "em@mail.at"
+  },
+  "ownerDetails": {
+    "firstName": "Imie",
+    "surName": "Wlasciciel"
+  }
+}]
+
+module.exports = settings
